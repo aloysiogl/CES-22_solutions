59afbeb7a0e6dac047928e9ee37b1b2ebd487bcc
aloysio <aloysiogl@gmail.com>
2018-03-05 11:31:37 -0300

    Finished exercise number 11.22.5

---
+++ Week2/ex11.22.5/vectors.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+
+import sys
+
+
+def test(did_pass):
+    """ Print the result of a test. """
+
+    linenum = sys._getframe(1).f_lineno
+
+    # Get the caller's line number.
+
+    if did_pass:
+        msg = "Test at line {0} ok.".format(linenum)
+    else:
+        msg = ("Test at line {0} FAILED.".format(linenum))
+    print(msg)
+
+
+def add_vectors(vect1, vect2):
+    """This function adds vectors"""
+    sum = [0]*len(vect1)
+
+    for i in range(0, len(vect1)):
+        sum[i] = vect1[i] + vect2[i]
+
+    return sum
+
+
+# Tests
+
+
+test(add_vectors([1, 1], [1, 1]) == [2, 2])
+test(add_vectors([1, 2], [1, 4]) == [2, 6])
+test(add_vectors([1, 2, 1], [1, 4, 3]) == [2, 6, 4])
