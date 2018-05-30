eb00efd24ac00aaa50935f678aa328424ef33dc0
aloysio <aloysiogl@gmail.com>
2018-05-29 22:54:32 -0300

    Class 10 adjustments to the client.

--- Bimester2/Class10/ChatClient.py
+++ Bimester2/Class10/ChatClient.py
@@ -8,20 +8,37 @@ import threading
 HOST = '127.0.0.1'
 PORT = 21567
 BUFSIZ = 1024
+name = ''
 
+
+# Socket initialization
 tcpCliSock = socket(AF_INET, SOCK_STREAM)
 ADDR = (HOST, PORT)
 tcpCliSock.connect(ADDR)
 host=tcpCliSock.getsockname()
 
+# Boolean to format enters
+enter = True
+
 
 def listener():
     """Method for listening to new messages"""
+
+    global enter, name
+
+    # First data
+    data = tcpCliSock.recv(BUFSIZ)
+    print(data.decode('utf-8'))
+
     while True:
         data = tcpCliSock.recv(BUFSIZ)
         if not data:
             break
-        print(data.decode('utf-8'))
+        if enter:
+            print(data.decode('utf-8') + "\n" + name + ">")
+            enter = False
+        else:
+            print(data.decode('utf-8') + "\n" + name + ">")
 
 
 if __name__ == "__main__":
@@ -29,7 +46,6 @@ if __name__ == "__main__":
     threading.Thread(target=listener).start()
 
     first_input = True
-    name = ''
 
     while True:
         if first_input:
@@ -37,7 +53,8 @@ if __name__ == "__main__":
             data = name.encode('utf-8')
             first_input = False
         else:
-            data = (input('%s> ' % name)).encode('utf-8')
+            data = (input('%s>\n' % name)).encode('utf-8')
+            enter = True
         if not data:
             break
         tcpCliSock.send(data)
